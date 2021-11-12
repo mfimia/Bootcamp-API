6ee0a0989e7e9c82d944d167ef320a178bec6b30
Martin Fimia <martin.fimia.osuna@hotmail.com>
2021-11-12 20:33:40 +0100

    controller file created with our methods (and specs) and exported to router file

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -1,34 +1,20 @@
-import  express  from "express";
+import express from "express";
+import {
+  getBootcamp,
+  getBootcamps,
+  createBootcamp,
+  updateBootcamp,
+  deleteBootcamp,
+} from "../controllers/bootcamps.js";
+
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  // I can send data in, for example, an object and express would automatically convert it to JSON.
-  // No need to use JSON.stringify. However, we would do res.json instead of res.send
-  // res.send("Hello from express");
-  // Setting up successful request (200)
-  res.status(200).json({ success: true, msg: "Show all bootcamps" });
-});
+router.route("/").get(getBootcamps).post(createBootcamp);
 
-router.get("/:id", (req, res) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Show bootcamp ${req.params.id}` });
-});
-
-router.post("/", (req, res) => {
-  res.status(200).json({ success: true, msg: "Create new bootcamp" });
-});
-
-router.put("/:id", (req, res) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Update bootcamp ${req.params.id}` });
-});
-
-router.delete("/:id", (req, res) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Delete bootcamp ${req.params.id}` });
-});
+router
+  .route("/:id")
+  .get(getBootcamp)
+  .put(updateBootcamp)
+  .delete(deleteBootcamp);
 
 export default router;
