362e67c1581aa9fe5a76f2ebf79240b2de82500f
Martin Fimia <martin.fimia.osuna@hotmail.com>
2021-11-12 15:37:34 +0100

    created basic structure of all routes in server file

--- server.js
+++ server.js
@@ -1,13 +1,39 @@
-// import { Express } from "express";
-// import { dotenv } from "dotenv";
 import express from "express";
 import { config } from "dotenv";
+// import { restart } from "nodemon";
 
 // Load env (environment) vars
 // We take them from config.env in our config folder
 config({ path: "./config/config.env" });
 
 const app = express();
+
+app.get("/api/v1/bootcamps", (req, res) => {
+  // I can send data in, for example, an object and express would automatically convert it to JSON.
+  // No need to use JSON.stringify. However, we would do res.json instead of res.send
+  // res.send("Hello from express");
+  // Setting up successful request (200)
+  res.status(200).json({ success: true, msg: "Show all bootcamps" });
+});
+
+app.get("/api/v1/bootcamps/:id", (req, res) => {
+  res.status(200).json({ success: true, msg: `Show bootcamp ${req.params.id}` });
+});
+
+app.post("/api/v1/bootcamps", (req, res) => {
+  res.status(200).json({ success: true, msg: "Create new bootcamp" });
+});
+
+app.put("/api/v1/bootcamps/:id", (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Update bootcamp ${req.params.id}` });
+});
+
+app.delete("/api/v1/bootcamps/:id", (req, res) => {
+  res.status(200).json({ success: true, msg: `Delete bootcamp ${req.params.id}` });
+});
+
 
 const PORT = process.env.PORT || 5000;
 
