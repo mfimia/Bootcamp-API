a127810a9588c5ad553ffb02c8c1800d9cdbf274
Martin Fimia <martin.fimia.osuna@hotmail.com>
2021-11-17 10:06:32 +0100

    prettier

--- utils/geocoder.js
+++ utils/geocoder.js
@@ -2,17 +2,14 @@ import NodeGeocoder from "node-geocoder";
 // import { config } from "dotenv";
 
 const options = {
-    
-  provider: 'mapquest',
-//   provider: process.env.GEOCODER_PROVIDER,
+  provider: "mapquest",
+  //   provider: process.env.GEOCODER_PROVIDER,
   httpAdapter: "https",
-  apiKey: 'NRcYOm46gAHsrs9dGau9eAl48GCR66db',
-//   apiKey: process.env.GEOCODER_API_KEY,
+  apiKey: "NRcYOm46gAHsrs9dGau9eAl48GCR66db",
+  //   apiKey: process.env.GEOCODER_API_KEY,
   formatter: null,
 };
 
-console.log(options.provider)
-
 const geocoder = NodeGeocoder(options);
 
-export default geocoder
+export default geocoder;
