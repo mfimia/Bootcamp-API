5b87cccf17c0f1e7f2212b26c0dbc43f1664b5ac
Martin Fimia <martin.fimia.osuna@hotmail.com>
2021-11-17 08:58:37 +0100

    asyncHandler now handling all asynchronous functions in controller

--- middleware/async.js
+++ middleware/async.js
@@ -1,5 +1,7 @@
 // Code snippet taken from async/await educational webpage(acuriousanimal.com)
 // This function receives a function and returns a function with 3 parameters that returns a promise
+// Created this function to receive all our asynchronous code and handle it in this format
+// Takes a function, whcih takes parameters, triggers the function with the parameters and adds a catch block which applies "next"
 const asyncHandler = (fn) => (req, res, next) =>
   Promise.resolve(fn(req, res, next)).catch(next);
 
